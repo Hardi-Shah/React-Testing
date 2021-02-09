61fa6be716c7a4bb7d9228a80c0f359bf310fad0
hardi shah <hardishahhardi@gmail.com>
2021-02-09 18:18:48 +0530

    Initial Commit

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-    presets:[
+    "presets":[
         "@babel/preset-env",
         "@babel/preset-react"
     ],
-    exclude: [/node_modules/],
+    "plugins": [
+    [
+        "@babel/plugin-transform-runtime",
+        {
+          "regenerator": true
+        }
+      ]
+    ]
 }
